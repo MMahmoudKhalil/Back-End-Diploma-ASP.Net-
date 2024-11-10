444444291618e2d8abbbcf358645399c23475680
MMahmoudKhalil <m.mahmoudkhalil98@gmail.com>
2024-11-10 19:03:07 +0300

    Add files via upload

---
+++ Arrey/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Arrey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArr = { 0, 0, 0, 0, 0 };
+            myArr[0] = 100;
+            myArr[^1] = 1000;
+            
+            Console.WriteLine(myArr[0]);
+            Console.WriteLine(myArr[^1]);
+            Console.WriteLine(myArr[2]);
+        }
+    }
+}

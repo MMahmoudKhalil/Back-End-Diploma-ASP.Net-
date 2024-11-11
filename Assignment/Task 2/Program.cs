b5c2ce221ba3862169c1ff472d81c8362cf52850
MMahmoudKhalil <m.mahmoudkhalil98@gmail.com>
2024-11-11 14:50:05 +0300

    Add files via upload

---
+++ Assignment/Task 2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 50;
+            int y = 30;
+            Console.WriteLine($"Equation = {x} + {y} = {x + y :d}");
+        }
+    }
+}

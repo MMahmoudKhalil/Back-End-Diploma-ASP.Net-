cb8b27cd85d77f378e2e4986c458800b6b82300d
MMahmoudKhalil <m.mahmoudkhalil98@gmail.com>
2024-11-11 14:59:56 +0300

    Add files via upload

---
+++ Assignment1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Laundrey");
+            double Tax = 0.06 ;
+            Console.Write("Enter the Number of small : " );
+            int number1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the Number of large : ");
+            int number2 = int.Parse(Console.ReadLine());
+            double a = number1*25 + number2*35;
+            double b = a * Tax;
+            double Cost = a + b;    
+            Console.WriteLine("Price per small room : $25");
+            Console.WriteLine("Price per large room : $35");
+            Console.WriteLine($"Cost : ${number1*25 + number2*35}");
+            Console.WriteLine($"Tax : {Tax} ==> Cost *0.6" );
+            Console.WriteLine("========================================");
+            Console.WriteLine($"Total estimate : {Cost}");
+            Console.WriteLine("This estimate is valid for 30 days");
+            
+                
+        }
+    }
+}
